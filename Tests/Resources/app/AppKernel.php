0a903ebbabd2a7f80328792e942675b5ee1c32cc
Chris Bednarczyk <krzysztof@tourradar.com>
2017-01-15 04:04:26 +0100

    testing

---
+++ Tests/Resources/app/AppKernel.php
@@ -0,0 +1,23 @@
+<?php
+
+use Symfony\Cmf\Component\Testing\HttpKernel\TestKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends TestKernel
+{
+    public function configure()
+    {
+        $this->requireBundleSets(array(
+            'default',
+        ));
+
+        $this->addBundles(array(
+            new \Symfony\Cmf\Bundle\MyBundle\CmfMyBundle(),
+        ));
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config.php');
+    }
+}
