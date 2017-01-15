621a263d33f810a548dafb2a53f7f1a77d2f1925
Chris Bednarczyk <chris@tourradar.com>
2017-01-15 05:47:53 +0100

    annotattions

--- Tests/Resources/app/autoload.php
+++ Tests/Resources/app/autoload.php
@@ -1,5 +1,11 @@
 <?php
 // File: Tests/app/autoload.php
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
+
 
 $loader = require __DIR__.'/../../../vendor/autoload.php';
 require __DIR__.'/AppKernel.php';
+
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+return $loader;
