768aff81a5841c9b57ea53170a10247fbf306bb1
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2021-01-30 19:45:10 +0100

    [FIX] Treebuilder, pass root in class definition

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('bordeux_geo_name');
+        $treeBuilder = new TreeBuilder('bordeux_geo_name');
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
