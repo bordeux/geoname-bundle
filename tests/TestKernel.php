be7076439598a231a9d8a6f464b71d7266cde0f9
Chris Bednarczyk <chris@tourradar.com>
2022-05-13 21:45:48 +0200

    Fix kernel

--- tests/TestKernel.php
+++ tests/TestKernel.php
@@ -2,23 +2,31 @@
 
 namespace Bordeux\Bundle\GeoNameBundle\Tests;
 
-
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
 class TestKernel extends Kernel
 {
-    use MicroKernelTrait;
-
     public function boot(): void
     {
         date_default_timezone_set('UTC');
         parent::boot();
     }
 
-
     public function getProjectDir(): string
     {
         return __DIR__;
+    }
+
+    public function registerBundles(): iterable
+    {
+        yield new \Symfony\Bundle\FrameworkBundle\FrameworkBundle();
+        yield new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle();
+        yield new \Bordeux\Bundle\GeoNameBundle\BordeuxGeoNameBundle();
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getProjectDir() . '/config.yaml');
     }
 }
--- tests/config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Bordeux\Bundle\GeoNameBundle\BordeuxGeoNameBundle::class => ['all' => true],
-];
