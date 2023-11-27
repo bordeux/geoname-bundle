367c716fec1f1f11f79b8d4ed08706a0955f24fc
treeindark <treeindark@gmail.com>
2023-11-27 13:41:15 +0100

    Update GeoName.php

--- src/Entity/GeoName.php
+++ src/Entity/GeoName.php
@@ -10,7 +10,6 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity()]
 #[ORM\Table(name: 'geo__country')]
 #[ORM\Index(name: 'geoname_country_search_idx', columns: ['name', 'iso'])]
-
 class Country implements Stringable
 {
     #[ORM\Id]
